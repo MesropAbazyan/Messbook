b9bb3237f2607b2a7f40dfacef0b621415fe8c2f
Mesrop Abazyan <107545114+MesropAbazyan@users.noreply.github.com>
2023-03-11 00:00:22 +0400

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,28 +1,32 @@
+import Topbar from '../../components/topbar/Topbar';
 import './Login.css';
 
 function Login() {
   return (
-    <div className='login'>
-      <div className="loginWrapper">
-        <div className="loginLeft">
-          <h3 className='loginLogo'>Messbook</h3>
-          <span className='loginDesc'>
-            Connect with friends and the world around you on Messbook!
-          </span>
-        </div>
-        <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder='Email' className="loginInput" />
-            <input placeholder='Password' className="loginInput" />
-            <button className="loginButton">Log In</button>
-            <span className='loginForgot'>Forgot Password?</span>
-            <button className="loginRegisterButton">
-              Create a New Account
-            </button>
+    <>
+      <Topbar />
+      <div className='login'>
+        <div className="loginWrapper">
+          <div className="loginLeft">
+            <h3 className='loginLogo'>Messbook</h3>
+            <span className='loginDesc'>
+              Connect with friends and the world around you on Messbook!
+            </span>
+          </div>
+          <div className="loginRight">
+            <div className="loginBox">
+              <input placeholder='Email' className="loginInput" />
+              <input placeholder='Password' className="loginInput" />
+              <button className="loginButton">Log In</button>
+              <span className='loginForgot'>Forgot Password?</span>
+              <button className="loginRegisterButton">
+                Create a New Account
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
